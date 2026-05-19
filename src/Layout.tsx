ee74555ea3c9e18f97334d1a8f272b62bc2068e8
UjjwalKumar02 <kumarujjwaldos@gmail.com>
2026-05-19 17:01:25 +0530

    update target section

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -5,7 +5,7 @@ import Hero from "./components/Hero";
 import Nav from "./components/Nav";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
-import Target from "./components/Target";
+// import Target from "./components/Target";
 
 export default function Layout() {
   return (
