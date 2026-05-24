bfd26ad79664e257be285dc13d844fa55247bb75
UjjwalKumar02 <kumarujjwaldos@gmail.com>
2026-05-24 13:21:11 +0530

    update nav

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -2,9 +2,9 @@ export default function Nav() {
   return (
     <div className="bg-white flex justify-end gap-7 py-4 border-b border-gray-100">
       <div className="flex px-10 items-center gap-7 text-sm mt-1">
-        <a href="#about">About</a>
-        <a href="#projects">Projects</a>
-        <a href="#skills">Skills</a>
+        <a href="#about">about</a>
+        <a href="#projects">projects</a>
+        <a href="#skills">skills</a>
       </div>
     </div>
   );
